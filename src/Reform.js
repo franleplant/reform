5b209e1662a79c03679e9eb824814a0bf5e1e6e0
Fran Guijarro <franleplant@gmail.com>
2016-09-08 19:53:35 -0300

    TODOs

--- src/Reform.js
+++ src/Reform.js
@@ -22,6 +22,7 @@ interface ReformConfig {
   https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5/Constraint_validation
 */
 
+// TODO: docs!
 // TODO: test getValue
 // TODO: move example to a inner create react app app
 // TODO: test with bootstrap and other third party components
