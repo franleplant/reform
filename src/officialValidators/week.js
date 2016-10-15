35d4dd6139c9d6350778f5768c723ffeb22b8d0d
Alexey Romanov <romanalexey@gmail.com>
2016-10-15 22:22:22 +0300

    Dynamically calculate weeks in years
    Thanks to @babruix

--- src/officialValidators/week.js
+++ src/officialValidators/week.js
@@ -3,20 +3,29 @@
 import { parseWeek } from '../utils';
 
 // Example week: "2016-W33"
-// TODO: this needs to be improved since some years have 52 weeks
-const MAX_WEEK = 53
-export default function weekValidator(control) {
+function weekValidator(control) {
   // Special case for empty values. This is the job of the `required` validator
   if (!control.value) {
     return false;
   }
 
-  const value = control.value
-  const [year, week] = parseWeek(value)
+  const value = control.value;
+  const [year, week] = parseWeek(value);
 
   if (!year || !week) {
-    return true
+    return true;
   }
 
-  return !(0 < year && 1 <= week && week <= MAX_WEEK)
+  return !(0 < year && 1 <= week && week <= weeksInYear(year));
 }
+
+function weeksInYear(year) {
+  const d = new Date(year, 0, 1);
+  const isLeap = new Date(year, 1, 29).getMonth() === 1;
+
+  // Check for a Jan 1 that's a Thursday or a leap year that has a
+  // Wednesday jan 1. Otherwise year has 52 weeks.
+  return d.getDay() === 4 || isLeap && d.getDay() === 3 ? 53 : 52
+}
+
+export {weekValidator as default, weeksInYear}
