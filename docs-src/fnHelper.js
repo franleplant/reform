688473a8d2ef2fc13473487bd8281a8a2e535e3e
Fran Guijarro <franleplant@gmail.com>
2017-11-21 13:02:27 -0300

    fix doc generator

--- docs-src/fnHelper.js
+++ docs-src/fnHelper.js
@@ -5,7 +5,7 @@ const commentsHelper = require('./commentsHelper');
 
 
 function fnSignatureHelper(item) {
-  const params = item.parameters.map(param => `${param.name}: ${typeHelper(param.type)}`).join(', ');
+  const params = (item.parameters || []).map(param => `${param.name}: ${typeHelper(param.type)}`).join(', ');
   const signature = `(${params}): ${typeHelper(item.type)}`;
 
   // Special case for inline, anonymous function signaruters in interfaces
